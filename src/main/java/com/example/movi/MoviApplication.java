756c2c2537809202c2a86c779b1296112be81d78
dope66 <prince628@naver.com>
2023-03-03 22:37:20 +0900

    first commit

---
+++ src/main/java/com/example/movi/MoviApplication.java
@@ -0,0 +1,13 @@
+package com.example.movi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviApplication.class, args);
+	}
+
+}
